ad997b6209ddf25f76c5611c82a04b76b86f36a2
sanghyeon <uphyeon@kakao.com>
2024-02-20 15:42:31 +0900

    :package: add: add prettier & tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-
 function App() {
-  return (
-    <div>
-
-    </div>
-  )
+  return <h1 className="text-3xl font-bold underline text-amber-300">Hello world!</h1>;
 }
 
-export default App
+export default App;
